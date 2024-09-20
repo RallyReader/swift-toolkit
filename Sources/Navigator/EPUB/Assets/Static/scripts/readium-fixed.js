cb6d71c49a6813d0e0c97619dc7a23dc918dfe29
Mihai <ghete.mihai@gmail.com>
2024-09-20 13:08:57 +0300

    Properly set the top offset for visible area containers

--- Sources/Navigator/EPUB/Assets/Static/scripts/readium-fixed.js
+++ Sources/Navigator/EPUB/Assets/Static/scripts/readium-fixed.js
@@ -1693,7 +1693,13 @@ function DecorationGroup(groupId, groupName) {
     let viewportWidth = window.innerWidth;
     let visibleAreaLeft = pageIndex * viewportWidth; // Each page is one viewport width in size
     let visibleAreaId = "visible-area-".concat(pageIndex);
-    const yOffset = 0; //window.innerHeight - document.documentElement.scrollHeight;
+
+    //const yOffset = 0; //window.innerHeight - document.documentElement.scrollHeight;
+    let yOffset = window.innerHeight - document.documentElement.scrollHeight;
+    (0,_utils__WEBPACK_IMPORTED_MODULE_1__.log)("yoffset: ".concat(yOffset));
+    if (yOffset < -100) {
+      yOffset = 0;
+    }
 
     // Check if the visible area for this page already exists
     let visibleArea = document.querySelector("#".concat(visibleAreaId));
@@ -1710,16 +1716,10 @@ function DecorationGroup(groupId, groupName) {
       visibleArea.style.pointerEvents = "none"; // Allow interactions to pass through
       document.body.appendChild(visibleArea);
     }
-
-    // if (
-    //   visibleArea.style.top != 0 &&
-    //   yOffset != 0 &&
-    //   visibleArea.style.top != yOffset
-    // ) {
-    //   log(`window top offset: ${yOffset}`);
-    //   visibleArea.style.top = `${yOffset}px`;
-    // }
-
+    if (visibleArea.style.top != 0 && yOffset != 0 && visibleArea.style.top != yOffset) {
+      (0,_utils__WEBPACK_IMPORTED_MODULE_1__.log)("window top offset: ".concat(yOffset));
+      visibleArea.style.top = "".concat(yOffset, "px");
+    }
     return visibleArea;
   }
 
