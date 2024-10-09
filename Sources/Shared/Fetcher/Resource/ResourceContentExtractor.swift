0a2cfbd853d660da554479a8195f6fc3ca118bb3
Mihai <ghete.mihai@gmail.com>
2024-10-09 11:33:52 +0300

    Remove content log

--- Sources/Shared/Fetcher/Resource/ResourceContentExtractor.swift
+++ Sources/Shared/Fetcher/Resource/ResourceContentExtractor.swift
@@ -63,7 +63,7 @@ class _HTMLResourceContentExtractor: _ResourceContentExtractor {
                     // Fuzi strips the HTML entities away when parsing the XML structure (we're missing characters like " ' etc)
                     // Getting the unescaped content before parsing it seems to solve the issue
                     
-                    print("content before unescaping: \(content)")
+//                    print("content before unescaping: \(content)")
                     
                     // Transform HTML entities into their actual characters.
                     let unescapedContent = try Entities.unescape(content
